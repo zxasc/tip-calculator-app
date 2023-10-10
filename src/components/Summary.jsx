352817803792132a7886f1292852364563c021c7
Paweł <55333847+zxasc@users.noreply.github.com>
2023-10-11 01:13:20 +0200

    add basic functionality to summary section

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,6 +1,28 @@
 import summary from '../styles/Summary.module.scss'
 
-export default function Summary() {
+export default function Summary({ appState, resetAppState }) {
+    const getTipAmount = () => {
+        const tipPercentage = appState.tip / 100;
+        const result = (tipPercentage * appState.bill) / appState.people
+        if(Number.isNaN(result)) {
+            return "0.00";
+        }
+        // Make sure that the number always has 2 decimal places
+        return (Math.round(result * 100) / 100).toFixed(2);
+    }
+
+    const getTotal = () => {
+        if(appState.bill <= 0 || appState.tip <= 0 || appState.people <= 0){
+            return "0.00"
+        }
+        const totalNoTip = appState.bill / appState.people;
+        const result = totalNoTip + Number(getTipAmount());
+        // Make sure that the number always has 2 decimal places
+        return (Math.round(result * 100) / 100).toFixed(2);
+    }
+
+    
+
     return (
         <section className={`inner-container ${summary.container}`}>
             <div className={summary.inner}>
@@ -8,18 +30,18 @@ export default function Summary() {
                     <h3>Tip Amount</h3>
                     <h4>/ person</h4>
                 </div>
-                <h2>$0.00</h2>
+                <h2>${getTipAmount()}</h2>
             </div>
 
             <div className={summary.inner}>
                 <div>
-                    <h3>Tip Amount</h3>
+                    <h3>Total</h3>
                     <h4>/ person</h4>
                 </div>
-                <h2>$0.00</h2>
+                <h2>${getTotal()}</h2>
             </div>
 
-            <button>RESET</button>
+            <button className={appState.bill > 0 ? summary.active : ""} onClick={resetAppState}>RESET</button>
         </section>
     )
 }
