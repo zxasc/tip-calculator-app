0e37faf51547765f8ce6624a278be318f44a94e5
Paweł <55333847+zxasc@users.noreply.github.com>
2023-10-10 20:48:09 +0200

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import '../styles/global.scss'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Initial</h1>
+    </>
+  )
+}
