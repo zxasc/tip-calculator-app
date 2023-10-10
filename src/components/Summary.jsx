cac33de165d6d2856a4d70ab8f700800173b46ec
Paweł <55333847+zxasc@users.noreply.github.com>
2023-10-10 23:49:43 +0200

    add summary section styling

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,7 +1,25 @@
+import summary from '../styles/Summary.module.scss'
+
 export default function Summary() {
     return (
-        <div className='inner-container'>
-            <h1>Summary</h1>
-        </div>
+        <section className={`inner-container ${summary.container}`}>
+            <div className={summary.inner}>
+                <div>
+                    <h3>Tip Amount</h3>
+                    <h4>/ person</h4>
+                </div>
+                <h2>$0.00</h2>
+            </div>
+
+            <div className={summary.inner}>
+                <div>
+                    <h3>Tip Amount</h3>
+                    <h4>/ person</h4>
+                </div>
+                <h2>$0.00</h2>
+            </div>
+
+            <button>RESET</button>
+        </section>
     )
 }
