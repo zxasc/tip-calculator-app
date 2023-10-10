ea16aa91aafe061669ef42aa6a03320e99deb7fc
Paweł <55333847+zxasc@users.noreply.github.com>
2023-10-11 01:12:52 +0200

    add basic functionality to tip input

--- src/components/TipInput.jsx
+++ src/components/TipInput.jsx
@@ -1,18 +1,22 @@
 import formInput from '../styles/FormInputs.module.scss'
 import formSelector from '../styles/FormSelector.module.scss'
 
-export default function TipInput() {
+export default function TipInput({ setTip }) {
+    const handleTipChange = (value) => {
+        setTip(Number(value))
+    }
+
     return (
         <section className={formInput.container}>
             <label htmlFor='custom-tip'>Select Tip %</label>
 
             <div className={formSelector.button_container}>
-                <button type='button'>5%</button>
-                <button type='button'>10%</button>
-                <button type='button'>15%</button>
-                <button type='button'>20%</button>
-                <button type='button'>25%</button>
-                <input type="number" id="custom-tip" min={1} max={100} placeholder='Custom' />
+                <button type='button' onClick={() => handleTipChange(5)}>5%</button>
+                <button type='button' onClick={() => handleTipChange(10)}>10%</button>
+                <button type='button' onClick={() => handleTipChange(15)}>15%</button>
+                <button type='button' onClick={() => handleTipChange(25)}>25%</button>
+                <button type='button' onClick={() => handleTipChange(50)}>50%</button>
+                <input type="number" id="custom-tip" min={1} max={100} placeholder='Custom' onChange={() => handleTipChange(event.target.value)} />
             </div>
         </section>
     )
