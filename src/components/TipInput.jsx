e47858054aa46ba70c1e933ceb363322160222a2
Paweł <55333847+zxasc@users.noreply.github.com>
2023-10-10 23:15:39 +0200

    add tip selector and css styling

--- src/components/TipInput.jsx
+++ src/components/TipInput.jsx
@@ -1,5 +1,19 @@
+import formInput from '../styles/FormInputs.module.scss'
+import formSelector from '../styles/FormSelector.module.scss'
+
 export default function TipInput() {
     return (
-        <h1>TipInput</h1>
+        <section className={formInput.container}>
+            <label htmlFor='custom-tip'>Select Tip %</label>
+
+            <div className={formSelector.button_container}>
+                <button type='button'>5%</button>
+                <button type='button'>10%</button>
+                <button type='button'>15%</button>
+                <button type='button'>20%</button>
+                <button type='button'>25%</button>
+                <input type="number" id="custom-tip" min={1} max={100} placeholder='Custom' />
+            </div>
+        </section>
     )
 }
