960a49195c009f206a59afc19de4aa94528c0111
Paweł <55333847+zxasc@users.noreply.github.com>
2023-10-11 18:59:19 +0200

    add min to number input in bill component

--- src/components/BillInput.jsx
+++ src/components/BillInput.jsx
@@ -9,7 +9,7 @@ function BillInput({ currentBill, setBill }) {
     return (
         <section className={formInput.container}>
             <label htmlFor="bill-input">Bill{currentBill <= 0 && <span>Invalid amount</span>}</label>
-            <input className={currentBill <= 0 ? formInput.invalid : ""} id='bill-input' type="number" placeholder='0' onChange={handleBillChange} value={currentBill ?? ''} />
+            <input className={currentBill <= 0 ? formInput.invalid : ""} id='bill-input' type="number" min={1} placeholder='0' onChange={handleBillChange} value={currentBill ?? ''} />
             <img className={formInput.dollar_icon} src="/icons/icon-dollar.svg" alt='dollar icon'/>
         </section>
     )
